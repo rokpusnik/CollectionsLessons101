7a8890661c6a40a528403a5e0ce82b12a917f213
Rok Pušnik <rok.pusnik@conology.onmicrosoft.com>
2019-04-17 10:35:37 +0200

    lesson 1 commit

--- src/com/conology/Main.java
+++ src/com/conology/Main.java
@@ -2,6 +2,19 @@ package com.conology;
 
 public class Main {
     public static void main(String[]s) {
-        System.out.println("test");
+        Theatre theatre = new Theatre("Olympian", 8,12);
+    //    theatre.getSeats();
+        if (theatre.reserveSeat("E12")) {
+            System.out.println("alright!");
+        } else {
+            System.out.println("nah, already reserved mate");
+        }
+        if (theatre.reserveSeat("E12")) {
+            System.out.println("alright!");
+        } else {
+            System.out.println("nah, already reserved mate");
+        }
+        theatre.reserveSeat("A11");
+        theatre.reserveSeat("E12");
     }
 }
